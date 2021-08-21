8b052537f1dbde7b8a39dc58e319f9a02d56ce5a
ipegte93 <ipegte93@gmail.com>
2021-08-21 15:56:38 +0900

    install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo pacman -S - < pacman.list
 sudo systemctl enable lightdm
 
-yay -S orchis-theme-git polybar nerd-fonts-complete yadm
+yay -S orchis-theme-git polybar nerd-fonts-complete yadm google-chrome
 
 
 yadm clone https://github.com/ipegte93/dotfiles.git
