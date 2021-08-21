2b3c82087a25a3f05d6030509267741b5dc6e721
ipegte93 <ipegte93@gmail.com>
2021-08-21 15:36:16 +0900

    arch-base

--- arch-base.sh
+++ arch-base.sh
@@ -9,7 +9,7 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "ko_KR.EUC-KR EUC-KR" >> /etc/locale.gen
 echo "ko_KR.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
+locale-gen
 
 echo "arch" >> /etc/hostname
 
@@ -17,3 +17,17 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
 echo "127.0.0.1 arch.localdomain arch" >> /etc/hosts
 
+pacman -S networkmanager grub os-prober efibootmgr
+grub-install --efi-directory=/boot
+os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable networkmanager.service
+
+useradd -mG wheel ipegte93
+echo "root passwd"
+passwd
+echo "ipegte93 passwd"
+passwd ipegte93
+
+echo "You have to edit '/etc/sudoers'."
