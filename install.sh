84d74479462a49639757da696725031da968f39b
ipegte93 <ipegte93@gmail.com>
2021-08-23 00:59:48 +0900

    merge

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ../
+sudo rm -rf yay
 
 sudo pacman -S - < pacman.list
 sudo systemctl enable lightdm
 
-yay -S orchis-theme-git polybar nerd-fonts-complete google-chrome picom-tryone-git
+yay -S orchis-theme-git polybar nerd-fonts-complete google-chrome picom-tryone-git yadm
+yadm clone https://github.com/ipegte93/dotfiles.git
+yadm alt
