e802773d91ebfd58aae1fe9cacbcbd9faf292b9a
ipegte93 <ipegte93@gmail.com>
2021-08-25 00:18:32 +0900

    update

--- bspwm.sh
+++ bspwm.sh
@@ -10,5 +10,5 @@ cd ../
 sudo rm -rf yay
 
 yay -S polybar nerd-fonts-complete google-chrome picom-tryone-git yadm kime
-yadm clone https://github.com/ipegte93/dotfiles.git
+yadm clone -f https://github.com/ipegte93/dotfiles.git
 yadm alt
