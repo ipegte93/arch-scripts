3c6d281cab2ee9a532f1a012616a6386f24511c2
ipegte93 <ipegte93@gmail.com>
2021-08-24 23:45:18 +0900

    update

--- bspwm.sh
+++ bspwm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+sudo pacman -S - < pacman.list
+sudo systemctl enable lightdm
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ../
 sudo rm -rf yay
-
-sudo pacman -S - < pacman.list
-sudo systemctl enable lightdm
 
 yay -S polybar nerd-fonts-complete google-chrome picom-tryone-git yadm kime
 yadm clone https://github.com/ipegte93/dotfiles.git
