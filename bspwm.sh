7b3ab55b461b10d4f4e7e1f70e55494c7ec5da16
ipegte93 <ipegte93@gmail.com>
2021-08-26 18:27:29 +0900

    update

--- bspwm.sh
+++ bspwm.sh
@@ -11,6 +11,7 @@ sudo rm -rf yay
 
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh
+rm -rf install.sh
 
 yay -S polybar nerd-fonts-complete google-chrome picom-tryone-git yadm kime-git oh-my-zsh-git
 yadm clone -f https://github.com/ipegte93/dotfiles.git
