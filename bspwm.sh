9d222015450b1d0e3308343576f7c4af6a0ba0e4
ipegte93 <ipegte93@gmail.com>
2021-08-27 02:37:56 +0900

    update

--- bspwm.sh
+++ bspwm.sh
@@ -9,11 +9,10 @@ makepkg -si
 cd ../
 sudo rm -rf yay
 
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
-rm -rf install.sh
+yay -S polybar nerd-fonts-complete google-chrome picom-tryone-git yadm kime-git
 
-yay -S polybar nerd-fonts-complete google-chrome picom-tryone-git yadm kime-git oh-my-zsh-git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 yadm clone -f https://github.com/ipegte93/dotfiles.git
 yadm reset HEAD
 yadm alt
