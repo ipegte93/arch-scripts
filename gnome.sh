33e67d7a05445874dcba703cbe64370d0fc12cfa
ipegte93 <ipegte93@gmail.com>
2021-10-04 20:57:11 +0900

    add zsh

--- gnome.sh
+++ gnome.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-server gnome gnome-tweaks gnome-software-packagekit-plugin ibus-hangul grub-cusomizer noto-fonts-cjk ttf-jetbrains-mono archlinux-appstream-data
+sudo pacman -S xorg xorg-server gnome gnome-tweaks gnome-software-packagekit-plugin ibus-hangul grub-cusomizer noto-fonts-cjk ttf-jetbrains-mono archlinux-appstream-data zsh-completions
 
 sudo systemctl enable gdm.service
 
 sh yay.sh
 yay -S google-chrome google-chrome-shell-git visual-studio-code-bin authy
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
