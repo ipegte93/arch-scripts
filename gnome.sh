6d177f75acf77fe9be0f212b118de49069225bc4
ipegte93 <ipegte93@gmail.com>
2021-10-06 18:28:13 +0900

    add powerline

--- gnome.sh
+++ gnome.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-server gnome gnome-tweaks gnome-software-packagekit-plugin ibus-hangul grub-cusomizer noto-fonts-cjk ttf-jetbrains-mono archlinux-appstream-data zsh-completions
+sudo pacman -S xorg xorg-server gnome gnome-tweaks gnome-software-packagekit-plugin ibus-hangul grub-cusomizer noto-fonts-cjk ttf-jetbrains-mono archlinux-appstream-data zsh-completions powerline powerline-fonts
 
 sudo systemctl enable gdm.service
 
 sh yay.sh
-yay -S google-chrome google-chrome-shell-git visual-studio-code-bin authy
+yay -S --noconfirm google-chrome google-chrome-shell-git visual-studio-code-bin authy ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
