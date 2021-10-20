227542ae4a58e9d2bd51ab6f72709dded7ba719e
ipegte93 <ipegte93@gmail.com>
2021-10-21 03:52:09 +0900

    Done

--- gnome.sh
+++ gnome.sh
@@ -4,8 +4,10 @@ sudo pacman -S --noconfirm xorg xorg-server gnome gnome-tweaks gnome-software-pa
 
 sudo systemctl enable gdm.service
 
+sh ./special/Orchis.sh
 sh ./special/yay.sh
-yay -S --noconfirm google-chrome chrome-gnome-shell-git visual-studio-code-bin authy ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git tela-circle-icon-theme-git
+
+yay -S --noconfirm google-chrome chrome-gnome-shell-git visual-studio-code-bin authy ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git tela-circle-icon-theme-git vimix-cursors-git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
