1c433ab59009a67f503e93ca18f7399c39192b76
ipegte93 <ipegte93@gmail.com>
2021-11-19 04:11:20 +0900

    Done

--- Arch/gnome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --noconfirm xorg xorg-server gnome gnome-tweaks gnome-software-packagekit-plugin ibus-hangul grub-customizer noto-fonts-cjk ttf-jetbrains-mono archlinux-appstream-data
-
-sudo systemctl enable gdm.service
-sudo -u gdm gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
-
-sh yay.sh
-yay -S --noconfirm google-chrome chrome-gnome-shell-git visual-studio-code-bin authy
--- Arch/mouse-acceleration-1.sh
+++
@@ -1,7 +0,0 @@
-echo 'Section "InputClass"
-	Identifier "My Mouse"
-	Driver "libinput"
-	MatchIsPointer "yes"
-	Option "AccelProfile" "flat"
-	Option "AccelSpeed" "0"
-EndSection' > /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
