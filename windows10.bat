9d6bb90dcb4e1d60f8af1a8fad5d23498248eb68
ipegte93 <71699228+ipegte93@users.noreply.github.com>
2021-12-08 00:52:56 +0900

    Update windows10.bat

--- windows10.bat
+++ windows10.bat
@@ -21,4 +21,7 @@ rustup component add rust-src rust-analysis
 :: sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 :: sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
+:: Turn off Windows Defender
+sudo REG ADD "hklm\software\policies\microsoft\windows defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+
 echo "You have to install Chipset driver and Graphic card driver. And then reboot."
