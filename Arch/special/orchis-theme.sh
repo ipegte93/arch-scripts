abad2a43cebf6d54cca5b8886f6b05b0f50f6ce7
ipegte93 <71699228+ipegte93@users.noreply.github.com>
2022-02-04 23:26:17 +0900

    Update orchis-theme.sh

--- Arch/special/orchis-theme.sh
+++ Arch/special/orchis-theme.sh
@@ -4,6 +4,6 @@ sudo pacman -S --noconfirm gtk-engine-murrine gnome-themes-extra sassc
 
 git clone https://github.com/vinceliuice/Orchis-theme.git
 
-sh ./Orchis-theme/install.sh -t grey -c light
+sh ./Orchis-theme/install.sh -t grey -c light --tweaks compact
 
 rm -rf Orchis-theme
