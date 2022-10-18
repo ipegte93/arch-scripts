734591873bb85595304fb9a4a19c6ececaaa5420
ipegte3 <ipegte93@gmail.com>
2022-10-19 06:08:19 +0900

    Edit mac install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -7,4 +7,12 @@ brew install $LIST
 CASK=""
 CASK+="linearmouse "
 CASK+="google-chrome "
+CASK+="iterm2"
+CASK+="visual-studio-code "
+CASK+="obsidian "
+CASK+="veracrypt "
+CASK+="discord "
+CASK+="authy "
+CASK+="reaper "
+CASK+="spotify "
 brew install --cask $CASK
